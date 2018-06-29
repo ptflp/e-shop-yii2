5df58810485a56ca190ec850de116cebd1826930
ptflp <filippov.pa@s-vfu.ru>
2018-06-29 21:16:47 +0900

    fixed models extending

--- models/Category.php
+++ models/Category.php
@@ -3,12 +3,12 @@
 namespace app\models;
 
 use Yii;
-use yii\base\Model;
+use yii\db\ActiveRecord;
 
 /**
  * ContactForm is the model behind the contact form.
  */
-class Category extends Model
+class Category extends ActiveRecord
 {
 	public static function tableName()
 	{
--- models/Product.php
+++ models/Product.php
@@ -3,7 +3,7 @@
 namespace app\models;
 
 use Yii;
-use yii\base\Model;
+use yii\db\ActiveRecord;
 
 /**
  * ContactForm is the model behind the contact form.
